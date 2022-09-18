245ecaf2110e6b3ecb6f1611f18cc541e9bdefb4
Kamaludin Khoir <khoirkamaludin@gmail.com>
2022-09-18 22:01:05 +0700

    add search function

--- js/apiCalls.js
+++ js/apiCalls.js
@@ -6,12 +6,29 @@ function getAllWords(callbackSuccess, callbackError) {
     xhttp.onload = function () {
         if (xhttp.status === 200) {
             const data = JSON.parse(xhttp.responseText);
-            callbackSuccess(data);
+            callbackSuccess(data.words);
         } else {
             callbackError();
         }
     };
 
     xhttp.open("GET", apiUrl+"undakusukbasa", true);
+    xhttp.send();
+}
+
+
+function getWordsBySubstring(substring, callbackSuccess, callbackError) {
+    const xhttp = new XMLHttpRequest();
+    
+    xhttp.onload = function () {
+        if (xhttp.status === 200) {
+            const data = JSON.parse(xhttp.responseText);
+            callbackSuccess(data);
+        } else {
+            callbackError();
+        }
+    };
+
+    xhttp.open("GET", apiUrl+"undakusukbasa/"+substring, true);
     xhttp.send();
 }
